939c46bd38526cfae7f1f676cf10f2f5bf240bc5
xingobar <gary840227@gmail.com>
2022-11-20 22:16:07 +0800

    feat: 取得特定的 todo list
    1. 取得特定的 todo list

--- todolist/views.py
+++ todolist/views.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from todolist.models import Todo
 from todolist.serializer import TodoSerializer
+from rest_framework.exceptions import NotFound
 
 
 class TodoViewSet(ViewSet):
@@ -9,4 +10,12 @@ class TodoViewSet(ViewSet):
     def list(self, request):
         todos = Todo.objects.all()
         serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk: int):
+        if Todo.objects.filter(id=pk).exists() is False:
+            raise NotFound()
+
+        todo = Todo.objects.filter(id=pk).first()
+        serializer = TodoSerializer(todo)
         return Response(serializer.data)
